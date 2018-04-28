deba5b0e288d83ad6dd9d79527a460851203e29a
Nidhi Goyal <nidhig@hike.in>
2018-04-29 00:12:13 +0530

    Add Marvel Api service class

--- Marvel/API Service/MarvelAPIService.swift
+++ Marvel/API Service/MarvelAPIService.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import Moya
+
+enum MarvelAPI {
+    case showCharacters(String?)
+    case showCharacterDetails(String)
+}
+
+extension MarvelAPI: TargetType {
+    var baseURL: URL { return URL(string: "")! }
+    
+    
+    var path: String {
+        switch self {
+        case .showCharacters:
+            return "/v1/public/characters"
+        case .characterDetails(let characterId):
+            return "/v1/public/characters/\(characterId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .showCharacters, .characterDetails:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        return .request
+    }
+    
+    var sampleData: Data {
+        switch self {
+        default:
+            return Data()
+        }
+    }
+}
